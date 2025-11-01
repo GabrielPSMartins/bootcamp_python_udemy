c09f609310a4fd0f6e8cbd516faa59c08e046fb5
GabrielPSMartins <martins80gabriel@gmail.com>
2025-11-01 11:59:29 -0300

    Iniciano o módulo de Ciência de Dados

---
+++ aulas/ciencias_de_dados/aula01.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+dados = pd.read_csv('Fifa/fifa.csv')
+
+print(dados)
+
+
+print("-----------------------------------------------------")
+# Mostra as 5 primeiras seções, mas posso colocar entre Parenteses a quantidade
+print(dados.head())
+
+print("-----------------------------------------------------")
+# Mostra as 5 últimas seções
+print(dados.tail())
+
+print("-----------------------------------------------------")
+dados.columns # mostra as colunas
+print(dados.columns)
+
+print("-----------------------------------------------------")
+# Mostra a quantidade de linhas
+print(dados.index)
+
+print("-----------------------------------------------------")
+print(dados[['Name', 'Nationality']])
+
+print("-----------------------------------------------------")
+#Localiza as informações conforme index enviado
+print(dados.iloc[0:4])
